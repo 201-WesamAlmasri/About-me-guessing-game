7b6bae78e5ecfbf695d484fefacef9d0dc3e7d63
Wesam Al-Masri <mr0virus@gmail.com>
2021-02-16 12:34:56 +0200

    adding loops to verify the input and adding two more question with using arrays and loops

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # About-me-guessing-game
+
+## Description
+
+This is a website about me with a guessing game.
+
+The site has a sections about me, education, experience and top ten cities that I want to visit
+
+- The site will ask you about your name, so that it will interact with you as a human
+- Five question with the answer yes or no (you can answer with y or n) no case sensitive, but you have to answer with one of these answers or you can't go to the next question
+- One question to guess a number from 1 to 100 and you have 4 attempts
+- One question with multiple answers
+- At the end of the game you will be given a score
--- js/app.js
+++ js/app.js
@@ -1,62 +1,148 @@
 'use strict';
 
+// Asking the user for his name
 let userName = prompt('Enter your name please.');
 console.log('The user name is ' + userName);
 alert('Hello ' + userName + ', Welcome to my webiste.');
 
-let wantToPlay = prompt('Do you want to play a guessing game with us? y/yes or n/no');
-console.log('The user wants to play a game? ' + wantToPlay);
+let points = 0;
 
-if(wantToPlay.toLowerCase() === 'y' || wantToPlay.toLowerCase() ==='yes'){
-  alert('good choice ' + userName + ' you will enjoy this with us' );
-} else if(wantToPlay.toLowerCase() === 'n' || wantToPlay.toLowerCase() ==='no'){
-  alert('Why you do not want to play with us ' + userName + ', it will be funny to play so lets start now.' );
-} else {
-  alert('Lets start the game now ' + userName);
+// Question 1
+let wantToPlay = '';
+
+while((wantToPlay !== 'y' && wantToPlay !== 'yes') && (wantToPlay !== 'n' && wantToPlay !== 'no')){
+  wantToPlay = prompt('Do you want to play a guessing game with us? y/yes or n/no').toLowerCase();
+  console.log('The user wants to play a game? ' + wantToPlay);
+
+  if(wantToPlay === 'y' || wantToPlay ==='yes'){
+    alert('good choice ' + userName + ' you will enjoy this with us' );
+    points = points + 1;
+  } else if(wantToPlay === 'n' || wantToPlay ==='no'){
+    alert('Why you do not want to play with us ' + userName + ', it will be funny to play so lets start now.' );
+  } else {
+    alert('Invalid  answer ' + userName);
+  }
 }
 
-let nicePerson = prompt('Do you think I am a nice person? y/yes or n/no');
-console.log('The user think I am a nice person? ' + nicePerson);
 
-if(nicePerson.toLowerCase() === 'y' || nicePerson.toLowerCase() ==='yes'){
-  alert('You are genius ' + userName + ' you was able to know that I am a nice person.' );
-} else if(nicePerson.toLowerCase() === 'n' || nicePerson.toLowerCase() ==='no'){
-  alert('Why do you think that ' + userName + ', Actually I am a very nice and lovely person' );
-} else {
-  alert('Actually I am a very nice and lovely person' + userName);
+// Question 2
+let nicePerson = '';
+
+while((nicePerson !== 'y' && nicePerson !== 'yes') && (nicePerson !== 'n' && nicePerson !== 'no')){
+  nicePerson = prompt('Do you think I am a nice person? y/yes or n/no').toLowerCase();
+  console.log('The user think I am a nice person? ' + nicePerson);
+
+  if(nicePerson === 'y' || nicePerson ==='yes'){
+    alert('You are genius ' + userName + ' you was able to know that I am a nice person.' );
+    points = points + 1;
+  } else if(nicePerson === 'n' || nicePerson ==='no'){
+    alert('Why do you think that ' + userName + ', Actually I am a very nice and lovely person' );
+  } else {
+    alert('Invalid answer ' + userName);
+  }
 }
 
-let traveledBefore = prompt('Have I ever traveled out of jordan before ? y/yes or n/no');
-console.log('The user answer if I traveled before? ' + traveledBefore);
+// Question 3
+let traveledBefore = '';
+while((traveledBefore !== 'y' && traveledBefore !== 'yes') && (traveledBefore !== 'n' && traveledBefore !== 'no')){
+  traveledBefore = prompt('Have I ever traveled out of jordan before ? y/yes or n/no').toLowerCase();
+  console.log('The user answer if I traveled before? ' + traveledBefore);
 
-if(traveledBefore.toLowerCase() === 'y' || traveledBefore.toLowerCase() ==='yes'){
-  alert('Yes, you are right ' + userName + ' I traveled before, I was in Russia this was an amazing experience to me.' );
-} else if(traveledBefore.toLowerCase() === 'n' || traveledBefore.toLowerCase() ==='no'){
-  alert('No ' + userName + ', Actually I traveled before to Russia and it  was an amazing experience to me' );
-} else {
-  alert('Actually I traveled before to Russia and it  was an amazing experience to me');
+  if(traveledBefore === 'y' || traveledBefore ==='yes'){
+    alert('Yes, you are right ' + userName + ' I traveled before, I was in Russia this was an amazing experience to me.' );
+    points = points + 1;
+  } else if(traveledBefore === 'n' || traveledBefore ==='no'){
+    alert('No ' + userName + ', Actually I traveled before to Russia and it  was an amazing experience to me' );
+  } else {
+    alert('Invalid answer ' + userName);
+  }
 }
 
-let loveToHelpOthers = prompt('Do you think I will mind to help other ? y/yes or n/no');
-console.log('The user think that I love to help other? ' + loveToHelpOthers);
 
-if(loveToHelpOthers.toLowerCase() === 'y' || loveToHelpOthers.toLowerCase() ==='yes'){
-  alert('Then you do not know mell well yet ' + userName + ', In fact I love to help others and this make me feel better. Don not hesitate to ask me anything if you need something. ' );
-} else if(loveToHelpOthers.toLowerCase() === 'n' || loveToHelpOthers.toLowerCase() ==='no'){
-  alert('I think you have an experience with people ' + userName + ', yes you are right. In fact I love to help others and this make me feel better. Don not hesitate to ask me anything if you need something.' );
-} else {
-  alert('In fact I love to help others and this make me feel better. Don not hesitate to ask me anything if you need something.');
+
+// Question 4
+let loveToHelpOthers = '';
+while((loveToHelpOthers !== 'y' && loveToHelpOthers !== 'yes') && (loveToHelpOthers !== 'n' && loveToHelpOthers !== 'no')){
+  loveToHelpOthers = prompt('Do you think I will mind to help other ? y/yes or n/no').toLowerCase();
+  console.log('The user think that I love to help other? ' + loveToHelpOthers);
+  if(loveToHelpOthers === 'y' || loveToHelpOthers ==='yes'){
+    alert('Then you do not know mell well yet ' + userName + ', In fact I love to help others and this make me feel better. Don not hesitate to ask me anything if you need something. ' );
+  } else if(loveToHelpOthers === 'n' || loveToHelpOthers ==='no'){
+    alert('I think you have an experience with people ' + userName + ', yes you are right. In fact I love to help others and this make me feel better. Don not hesitate to ask me anything if you need something.' );
+    points = points + 1;
+  } else {
+    alert('Invalid answer ' + userName);
+  }
 }
 
-let knowAboutProgramming = prompt('Do I have a previous experience with web development before the course from ASAC ? y/yes or n/no');
-console.log('The user think that I love to help other? ' + knowAboutProgramming);
 
-if(knowAboutProgramming.toLowerCase() === 'y' || knowAboutProgramming.toLowerCase() ==='yes'){
-  alert('I do not know how you could guess that ' + userName + ', you are right I have some experience with web development using python and if you want to see one of my project that I have made, send me a message.' );
-} else if(knowAboutProgramming.toLowerCase() === 'n' || knowAboutProgramming.toLowerCase() ==='no'){
-  alert('This is not true ' + userName + ', I have some experience with web development using python and if you want to see one of my project that I have made, send me a message.' );
-} else {
-  alert('This is not true ' + userName + ', I have some experience with web development using python and if you want to see one of my project that I have made, send me a message.' );
+// Question 5
+let knowAboutProgramming = '';
+while((knowAboutProgramming !== 'y' && knowAboutProgramming !== 'yes') && (knowAboutProgramming !== 'n' && knowAboutProgramming !== 'no')){
+  knowAboutProgramming = prompt('Do I have a previous experience with web development before the course from ASAC ? y/yes or n/no').toLowerCase();
+  console.log('The user think that I love to help other? ' + knowAboutProgramming);
+
+  if(knowAboutProgramming === 'y' || knowAboutProgramming ==='yes'){
+    alert('I do not know how you could guess that ' + userName + ', you are right I have some experience with web development using python and if you want to see one of my project that I have made, send me a message.' );
+    points = points + 1;
+  } else if(knowAboutProgramming === 'n' || knowAboutProgramming ==='no'){
+    alert('This is not true ' + userName + ', I have some experience with web development using python and if you want to see one of my project that I have made, send me a message.' );
+  } else {
+    alert('This is not true ' + userName + ', I have some experience with web development using python and if you want to see one of my project that I have made, send me a message.' );
+  }
 }
 
-alert('Finally thank you ' + userName + ', for trying the game with us and I hope you enjoyed it alot.' );
+// Question 6
+let randomNumber = Math.floor(Math.random()*100 + 1);
+console.log('the random number is ', randomNumber);
+
+for(let tryNumber = 1; tryNumber <= 4; tryNumber++){
+  let guessedNumber = prompt('I have a number in my mind from 1 to 100, guess the number. You have four attempts.');
+  console.log('The number that the user guessed is ' + guessedNumber);
+
+  if (guessedNumber < randomNumber){
+    alert('Attemp number ' + tryNumber + '. The number were too low,');
+    if(tryNumber === 4){
+      alert('You could not guess the number, my number was ' + randomNumber);
+    }
+  } else if(guessedNumber > randomNumber){
+    alert('Attemp number ' + tryNumber + '. The number were too high,');
+    if(tryNumber === 4){
+      alert('You could not guess the number, my number was ' + randomNumber);
+    }
+  } else {
+    alert('Attemp number ' + tryNumber + '. You guessed it, this is the number that I choosed ' + randomNumber);
+    points = points + 1;
+    break;
+  }
+}
+
+// Question 7 with multiple correct values
+let answers = ['habieb', 'naseem', 'waseem', 'nadeem'];
+let correct = false;
+
+for(let attemptNumber = 1; attemptNumber <= 6; attemptNumber++){
+  let brotherName = prompt('Can you guess on of my brothers name? you have 6 attempts. ').toLowerCase();
+  console.log('The name that the user guessed is ' + brotherName);
+
+  for(let j = 0; j < answers.length; j++){
+    if (brotherName === answers[j]){
+      correct = true;
+    } else {
+      correct = false;
+    }
+  }
+  if(correct){
+    points = points + 1;
+    alert('Attemp number ' + attemptNumber + '. You guessed it, this is one of my brothers name');
+    alert('I have four brothers and their names ' + `${answers[0]}, ${answers[1]}, ${answers[2]} and ${answers[3]}`);
+    break;
+  } else {
+    alert('Attemp number ' + attemptNumber + '. Wrong answer! ');
+    if(attemptNumber === 4){
+      alert('You could not guess any name. I have four brothers and their names ' + `${answers[0]}, ${answers[1]}, ${answers[2]} and ${answers[3]}`);
+    }
+  }
+}
+
+alert('Finally thank you ' + userName + ', your score is ' + points + '/7. Good luck and I hope you enjoyed the game it alot.' );
